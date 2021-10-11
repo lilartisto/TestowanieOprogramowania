188f8b6f3610b04577207cdb3442a40b5c1b6a85
nakielsh <hubert.nakielski@gmail.com>
2021-10-11 18:12:49 +0200

    Created Create and Read transactions

---
+++ proj1/src/main/java/Main.java
@@ -0,0 +1,24 @@
+import controller.Controller;
+import entity.Student;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+
+        Student student = new Student("Mariusz", "Pudzianowski", 3983763, "EITI", "Automatyka i Robotyka", 3);
+        Student student1 = new Student("John", "Cena", 123456, "Elektryczny", "Informatyka Stosowana", 5);
+
+        controller.saveStudent(student);
+        controller.saveStudent(student1);
+        controller.showAllStudents();
+        controller.searchController("indexNo", 3983763);
+        controller.searchController("firstName", "Mariusz");
+        controller.searchController("lastName", "Cena");
+        controller.searchController("faculty", "Elektryczny");
+
+        controller.closeSession();
+    }
+
+}
---
+++ proj1/src/main/java/controller/Controller.java
@@ -0,0 +1,57 @@
+package controller;
+
+import entity.Student;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import util.HibernateUtilities_5;
+
+import java.util.List;
+
+public class Controller {
+    Session session = null;
+    Transaction transaction = null;
+
+    public void openSession() {
+        if (session == null) {
+            session = HibernateUtilities_5.getSessionFactory().openSession();
+        }
+        transaction = session.beginTransaction();
+    }
+
+    public void closeSession() {
+        HibernateUtilities_5.getSessionFactory().close();
+    }
+
+    public void saveStudent(Student student) {
+        openSession();
+        session.save(student);
+        transaction.commit();
+        System.out.println("Saved student: " + student.getFirstName() + ' ' + student.getLastName());
+    }
+
+    public List<Student> showAllStudents() {
+        openSession();
+        System.out.print("Showing all students\n");
+        List<Student> studentList = session.createQuery("from Student", Student.class).list();
+        transaction.commit();
+        printList(studentList);
+        return studentList;
+    }
+
+
+    //first parameter is attribute name, eg. firstName
+    //second parameter is search value, eg. Mariusz
+    public List<Student> searchController(String attr, Object value) {
+        openSession();
+        System.out.print("Searching " + attr + " that is equal to " + value + "\n");
+        String query = "from Student where " + attr + "=:" + attr;
+        List<Student> studentList = session.createQuery(query, Student.class).setParameter(attr, value).list();
+        transaction.commit();
+        printList(studentList);
+        return studentList;
+    }
+
+    public void printList(List<Student> studentList) {
+        studentList.forEach(s -> System.out.print(s.toString() + '\n'));
+    }
+}

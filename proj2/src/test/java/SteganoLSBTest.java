fb536848916fec5d8b12fcdbe43ddde581ea48fd
nakielsh <hubert.nakielski@gmail.com>
2021-11-05 18:20:40 +0100

    added helping function, that checks if two last bits of pixels in encoded image are equal to secret image pixel bytes

--- proj2/src/test/java/SteganoLSBTest.java
+++ proj2/src/test/java/SteganoLSBTest.java
@@ -1,10 +1,28 @@
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class SteganoLSBTest {
 
+    //checks if last two bits of every pixel in encoded image are the same as secret image pixel bytes
+    private void are2latestEqual(BufferedImage encoded, BufferedImage secret) {
+        BufferedImage twoBitSecret = convertTo2bitImage(secret);
+
+        for (int w = 0; w < encoded.getWidth(); w++) {
+            for (int h = 0; h < encoded.getHeight(); h++) {
+                Color encColor = new Color(encoded.getRGB(w, h));
+                Color secColor = new Color(twoBitSecret.getRGB(w, h));
+
+                assertEquals(secColor.getRed(), convertFrom2BitTo8BitValue(encColor.getRed() % 4));
+                assertEquals(secColor.getGreen(), convertFrom2BitTo8BitValue(encColor.getGreen() % 4));
+                assertEquals(secColor.getBlue(), convertFrom2BitTo8BitValue(encColor.getBlue() % 4));
+                assertEquals(secColor.getAlpha(), convertFrom2BitTo8BitValue(encColor.getAlpha() % 4));
+            }
+        }
+    }
+
+    //converts image to converted image (8bit to 2bit values)
     private BufferedImage convertTo2bitImage(BufferedImage image) {
 
         for (int w = 0; w < image.getWidth(); w++) {
@@ -22,9 +40,15 @@ public class SteganoLSBTest {
         return image;
     }
 
+    //conversion from 8 to 2 bit value - returns int 0-255
     private int convertFrom8To2BitValue(int value) {
         int twoBit = Math.round(((float) value / 255) * 3);
         return Math.round(((float) twoBit / 3) * 255);
+    }
+
+    //conversion from 2 (0-3) to 8 bit value - returns int 0-255
+    private int convertFrom2BitTo8BitValue(int value) {
+        return Math.round(((float)value / 3) * 255 );
     }
 
 

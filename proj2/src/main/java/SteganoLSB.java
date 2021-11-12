5ee7e369aeeacee3150ce17c932757f047b53f8a
lilartisto <orginalartis@gmail.com>
2021-11-12 14:49:52 +0100

    SteganoLSB.decode method implemented

--- proj2/src/main/java/SteganoLSB.java
+++ proj2/src/main/java/SteganoLSB.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class SteganoLSB implements Stegano {
@@ -9,7 +10,40 @@ public class SteganoLSB implements Stegano {
 
     @Override
     public BufferedImage decode(BufferedImage encoded) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        if (encoded == null) {
+            throw new IllegalArgumentException("Passed image cannot be null");
+        }
+
+        return decodeImg(encoded);
+    }
+
+    private BufferedImage decodeImg(BufferedImage encoded) {
+        int width = encoded.getWidth();
+        int height = encoded.getHeight();
+        BufferedImage decodedImg = new BufferedImage(width, height, encoded.getType());
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                decodedImg.setRGB(x, y, decodeRGBPixel(decodedImg.getRGB(x, y)));
+            }
+        }
+
+        return decodedImg;
+    }
+
+    private int decodeRGBPixel(int pixel) {
+        Color pixelColor = new Color(pixel);
+
+        return new Color(
+                normalize2lsbTo8b(pixelColor.getRed()),
+                normalize2lsbTo8b(pixelColor.getGreen()),
+                normalize2lsbTo8b(pixelColor.getBlue()),
+                normalize2lsbTo8b(pixelColor.getAlpha())
+        ).getRGB();
+    }
+
+    private int normalize2lsbTo8b(int number) {
+        return (number % 4) * 255 / 3;
     }
 
 }
